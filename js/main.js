f782b7ecc41f94541709b923aac6b6b868044047
Aleksey <a.abrosimow2012@yandex.ru>
2021-11-30 20:34:27 +0300

    Финальная версия страницы

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@
 (function () {
   var headerButton = document.querySelector('.page-header__button');
   var headerButtonToggle = document.querySelector('.page-header__block');
+  var headerContact = document.querySelector('.page-header__box-contact');
+  var headerBoxToggle = document.querySelector('.page-header__block');
   var headerMenu = document.querySelector('.main-nav');
+  var headerMenuSub = document.querySelector('.main-nav__sub');
   var header = document.querySelector('.page-header');
   var popupOverlay = document.querySelector('.overlay');
   var pageBody = document.querySelector('.page-body');
@@ -48,6 +51,11 @@
       }
     });
   }
+  if (headerContact) {
+      headerContact.addEventListener('click', function () {
+        toggleMenu(headerMenuSub, 'main-nav__sub--block');
+      });
+  }
 
   document.addEventListener('keydown', function (evt) {
     if (evt.key === 'Escape') {
@@ -71,5 +79,95 @@
         behavior: 'smooth'
       });
     });
+  });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    if (document.querySelector('.team__swiper')) {
+      const readySwiper = new Swiper('.team__swiper', {
+        spaceBetween: 50,
+        navigation: {
+          prevEl: '.team__btn-prev',
+          nextEl: '.team__btn-next',
+        },
+        pagination: {
+          el: '.team__pagination',
+          clickable: true,
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          480: {
+            slidesPerView: 2,
+          },
+          750: {
+            slidesPerView: 3,
+          },
+          1000: {
+            slidesPerView: 4,
+          }
+        }
+      });
+    }
+
+    if (document.querySelector('.meeting__swiper')) {
+      const readySwiper = new Swiper('.meeting__swiper', {
+        autoHeight: true,
+        spaceBetween: 70,
+        navigation: {
+          prevEl: '.meeting__btn-prev',
+          nextEl: '.meeting__btn-next',
+        },
+        pagination: {
+          el: '.meeting__pagination',
+          clickable: true,
+        },
+        breakpoints: {
+          0: {
+            centeredSlides: true,
+            slidesPerView: 1,
+          },
+          600: {
+            slidesPerView: 2,
+          },
+          750: {
+            slidesPerView: 2,
+          },
+          1300: {
+            slidesPerView: 3,
+          }
+        }
+      });
+    }
+
+    if (document.querySelector('.partners__swiper')) {
+      const readySwiper = new Swiper('.partners__swiper', {
+        spaceBetween: 10,
+        navigation: {
+          prevEl: '.partners__btn-prev',
+          nextEl: '.partners__btn-next',
+        },
+        pagination: {
+          el: '.partners__pagination',
+          clickable: true,
+        },
+        breakpoints: {
+          0: {
+            spaceBetween: 10,
+            slidesPerView: 1,
+          },
+          480: {
+            slidesPerView: 2,
+          },
+          750: {
+            
+            slidesPerView: 3,
+          },
+          1300: {
+            slidesPerView: 5,
+          }
+        }
+      });
+    }
   });
 })();
